1d6b600253d8ad94779eb88980fbe0b2e748d64a
shadi <shd.yahoo.com>
2022-08-21 15:05:35 +0430

    greenweb

---
+++ greenweb/greenweb.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>هلدینگ دانش بنیان گرین وب -پیشرو درتحول دیجیتال کسب و کارها</title>
+    <link rel="stylesheet" href="css\style.css">
+
+</head>
+
+<body>
+    <!-- header -->
+    <div class="header-1">
+        <div class="header-1-row-1">
+
+            <div class="hdeader-1-row-1-right">
+                <div class="logo-1">
+                    <a href="https://greenweb.ir/" title="|  نوآوری در فناوری، هوشمندی در کسب و کار">
+                        <img src="https://greenweb.ir/wp-content/uploads/2022/06/GWlogo.png" data-src="" alt="هلدینگ دانش بنیان گرین وب" style="width: 200px" class="logo-gw" width="200" height="53"></a>
+                </div>
+            </div>
+
+            <div class="hdeader-1-row-1-left">
+                <!-- <div class="" style="margin-top:10px;margin-right:15px;"> -->
+                <div class="logo-left"><a class="cz-instagram" href="https://www.instagram.com/greenwebco/" rel="noreferrer" title="Instagram" aria-label="Instagram" target="_blank"><i class="fa fa-instagram"></i><span>Instagram</span></a><a rel="noreferrer" title="Linkedin" aria-label="Linkedin" target="_blank"><i class="fa fa-linkedin"></i><span>Linkedin</span></a><a class="cz-twitter" href="https://twitter.com/greenwebir" rel="noreferrer" title="Twitter" aria-label="Twitter" target="_blank"><i class="fa fa-twitter"></i><span>Twitter</span></a></div>
+                <!-- </div> -->
+                <div class="logo-left" style="margin-top:15px;margin-left:10px;"><a class="elm_icon_text" href="mailto:Info@GreenWeb.ir"><i class="fa czico-089-mail-1" style="font-size:22px;color:#79a342;margin-top:3px;margin-right:7px;float:left;"></i><span class="it_text ml10" style="color:rgba(58,58,58,0.7);font-weight:500;">Info@GreenWeb.ir</span></a></div>
+                <div class="logo-left" style="margin-top:15px;margin-left:20px;"><a class="elm_icon_text" href="tel:00982188853913"><i class="fa czico-122-phone-call" style="font-size:22px;color:#79a342;margin-top:2px;margin-right:5px;float:left;"></i><span class="it_text ml10" style="color:rgba(58,58,58,0.7);font-weight:500;">88853913 - 021</span></a></div>
+
+            </div>
+        </div>
+        <!-- row menu -->
+        <div class="header-1-row-2">
+
+            <div class="header-1-row-2-col-2">
+                <div class="ul-menu" style="margin-top:10px;margin-right:6px;margin-bottom:10px;margin-left:6px;"><i class=""><span></span></i>
+                    <ul class="list-inline" data-indicator="fa fa-angle-down" data-indicator2="fa fa-angle-right" style="touch-action: pan-y;">
+                        <li class="" data-sub-menu=""><a href="https://greenweb.ir/" data-title="صفحه اصلی"><span>صفحه اصلی</span></a></li>
+                        <li class="" data-sub-menu=""><a href="#" data-title="درباره ما" class="sf-with-ul"><span>درباره ما</span><i class="cz_indicator fa fa-angle-down"></i></a>
+                            <ul class="sub-menu" style="display: none;">
+                                <li class="" data-sub-menu=""><a href="https://greenweb.ir/about-us/" data-title="داستان گرین وب"><span>داستان گرین وب</span></a></li>
+                            </ul>
+                        </li>
+                        <li class="" data-sub-menu=""><a href="https://www.blog.greenweb.ir" data-title="بلاگ"><span>بلاگ</span></a></li>
+                        <li class="" data-sub-menu=""><a href="https://job.greenweb.ir" data-title="فرصت‌های شغلی"><span>فرصت‌های شغلی</span></a></li>
+                        <li class="" data-sub-menu=""><a href="https://greenweb.ir/contact-us/" data-title="تماس با ما"><span>تماس با ما</span></a></li>
+                        <li class="" style="display: none;"><a href="#" class="sf-with-ul"><span>&nbsp;<i class="fa czico-055-three cz-extra-menus" style="margin: 0px; display: none;"></i>&nbsp;</span></a>
+                            <ul class="sub-menu" style="display: none;"></ul>
+                        </li>
+                    </ul><i class=""></i>
+                </div>
+            </div>
+            <div class="header-1-row-2-col-1">
+                <div class="triangle-right">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- wrap -->
+
+    <rs-layer-wrap class="" style="position: absolute; display: block; pointer-events: auto; left: 550px; top: 104px; z-index: 6; visibility: visible;">
+        <rs-loop-wrap style="position:absolute;display:block;">
+            <rs-mask-wrap style="position: absolute; display: block; overflow: visible;">
+                <h1 id="slider-2-slide-4-layer-1" class="rs-layer" data-type="text" data-color="#fff" data-rsp_ch="on" data-xy="x:c;y:m;yo:-66px,-54px,-119px,-30px;" data-text="w:normal;s:80,66,50,30;l:90,74,56,34;ls:-4,-3,-2,-1;fw:800;a:center;" data-dim="minh:0px,none,none,none;" data-frame_999="o:0;st:w;" style="z-index: 6; font-family:vazir; height: auto; width: auto; color: rgb(255, 255, 255); text-decoration: none; white-space: nowrap; 
+                 min-height: 0px; min-width: 0px; max-height: none; max-width: none; text-align: center; line-height: 90px; letter-spacing: -4px; font-weight: 800; 
+                 font-size: 80px; transform-origin: 50% 50% 0px; opacity: 1; transform: translate(0px); visibility: visible;" data-idcheck="true" data-stylerecorder="true" data-initialised="true">نوآوری در فناوری<br> هوشمندی در کسب‌وکار
+                </h1>
+            </rs-mask-wrap>
+        </rs-loop-wrap>
+    </rs-layer-wrap>
+    <!-- wrap -->
+
+    <rs-layer-wrap class="" style="position: absolute; display: block; pointer-events: auto; left: 480px; top: 300px; z-index: 7; visibility: visible;">
+        <rs-loop-wrap style="position:absolute;display:block;">
+            <rs-mask-wrap style="position: absolute; display: block; overflow: visible;">
+                <rs-layer id="slider-2-slide-4-layer-2" data-type="text" data-rsp_ch="on" data-xy="x:c;yo:385px,413px,425px,376px;" data-text="w:normal;s:20,16,12,7;l:25,20,15,9;" data-frame_999="o:0;st:w;" style="z-index: 7; font-family: vazir; height: auto; width: auto; color: rgb(255, 255, 255); text-decoration: none; white-space: nowrap; 
+                 min-height: 0px; min-width: 0px; max-height: none; max-width: none; text-align: left; line-height: 25px; letter-spacing: 0px; font-weight: 400; font-size: 25px; 
+                 transform-origin: 50% 50% 0px; opacity: 1; transform: translate(0px); visibility: visible;" class="rs-layer" data-idcheck="true" data-stylerecorder="true" data-initialised="true">هلدینگ دانش بنیان گرین وب، صادرکننده برتر خدمات فناوری اطلاعات به اروپا<br>
+
+                </rs-layer>
+            </rs-mask-wrap>
+        </rs-loop-wrap>
+    </rs-layer-wrap>
+    <!-- video -->
+    <div class="div-video">
+        <video class="video" autoplay>
+            <source src="//www.greenweb.ir/wp-content/uploads/2021/03/sample.webm" type="video/mp4">
+        </video>
+    </div>
+    <!-- contain1 -->
+
+    <div class="row">
+
+        <div class="column">
+            <div class="row-1">
+
+                <div>
+                    <p style="text-align: center;"><img class="aligncenter wp-image-1602 size-full lazyDone" src="https://www.greenweb.ir/inc/uploads/2021/03/hosting-icon.png" data-src="" alt="گرین وب، خدمات زیرساخت هاستینگ" width="101" height="98"><strong><span style="font-size: 22px; color: #79a342;">خدمات زیرساخت هاستینگ</span></strong><span style="color: #58595b;"><br>
+                            <span style="color: #aaaaaa; font-size: 14px;"><strong>WEB HOSTING INFRASTRUCTURE</strong></span></span></p>
+                    <p style="text-align: center;"><span style="color: #aaaaaa;">سرویس‌های مطمئن و با کیفیت میزبانی وب در سطح<br>
+                            استانداردهای جهانی</span></p>
+                </div>
+                <div class="row-1-col-1"><a id="cz_14774" class="" href="https://www.greenweb.ir/web-hosting-infrastructure/" title="خدمات زیرساخت هاستینگ"><span>
+                            <strong>اطلاعات بیشتر</strong></span><b class="cz_btn_onhover"><strong>اطلاعات بیشتر</strong></b></a></div>
+            </div>
+
+        </div>
+        <div class="column">
+            <p style="text-align: center;"><img class="aligncenter wp-image-1600 size-full lazyDone" src="https://www.greenweb.ir/inc/uploads/2021/03/startup-icon.png" data-src="" alt="گرین وب، حمایت از استارت اپ ها" width="101" height="98"><strong><span style="font-size: 22px; color: #79a342;">حمایت از استارت اپ ها</span></strong><span style="color: #58595b;"><br>
+                    <span style="color: #aaaaaa; font-size: 14px;"><strong>ACCELERATOR &amp; STARTUPS SUPPORT</strong></span></span></p>
+            <p style="text-align: center;"><span style="color: #aaaaaa;">شتابدهی آنلاین به استارتاپ‌های نوآور با ارائه‌ی<br>
+                    خدماتی منحصر به فرد</span></p>
+        </div>
+        <div class="column">
+            <p style="text-align: center;"><img class="aligncenter wp-image-1892 size-full lazyDone" src="https://www.greenweb.ir/inc/uploads/2021/03/Dt-icon2.png" data-src="" alt="گرین وب، تحول دیجیتال کسب وکارها" width="101" height="98"><strong><span style="font-size: 22px; color: #79a342;">تحول دیجیتال کسب و کارها</span></strong><span style="color: #58595b;"><br>
+                    <span style="color: #aaaaaa; font-size: 14px;"><strong>DIGITAL TRANSFORMATION FOR BUSINESS</strong></span></span></p>
+            <p style="text-align: center;"><span style="color: #aaaaaa;">ادغام فناوری‌های دیجیتال در جهت هوشمندسازی<br>
+                    کسب و کارها</span></p>
+        </div>
+
+    </div>
+
+    
+
+    <!-- contain row 2 -->
+    <div class="row">
+        <div class="column">
+            <p style="text-align: center;"><img class="aligncenter wp-image-1609 size-full lazyDone" src="https://www.greenweb.ir/inc/uploads/2021/03/Blockchain-icon.png" data-src="" alt="گرین وب، سرویس های بلاکچین" width="101" height="98"><strong><span style="font-size: 22px; color: #79a342;">سرویس های بلاکچین</span></strong><span style="color: #58595b;"><br>
+                    <span style="color: #aaaaaa; font-size: 14px;"><strong>BLOCKCHAIN SERVICES</strong></span></span></p>
+            <p style="text-align: center;"><span style="color: #aaaaaa;">راه حل‌های کارآمد در ساختار و معماری اینترنت با تمرکز بر امنیت و کارایی سرویس ها</span></p>
+
+        </div>
+        <div class="column">
+            <p style="text-align: center;"><img class="aligncenter wp-image-1953 size-full lazyDone" src="https://www.greenweb.ir/inc/uploads/2021/03/Dt-icon.png" data-src="" alt="گرین وب، پردازش تصویر و هوش مصنوعی" width="101" height="98"><strong><span style="font-size: 22px; color: #79a342;">پردازش تصویر و هوش مصنوعی</span></strong><span style="color: #58595b;"><br>
+                    <span style="color: #aaaaaa; font-size: 14px;"><strong>IMAGE PROCESSING &amp; AI</strong></span></span></p>
+            <p style="text-align: center;"><span style="color: #aaaaaa;">طراحی و ساخت سیستم های مبتنی بر هوش مصنوعی<br>
+                    و بینایی ماشین</span></p>
+
+        </div>
+        <div class="column">
+            <p style="text-align: center;"><img class="aligncenter wp-image-1607 size-full lazyDone" src="https://www.greenweb.ir/inc/uploads/2021/03/cloud-services-icon.png" data-src="" alt="گرین وب، سرویس های ابری" width="101" height="98"><strong><span style="font-size: 22px; color: #79a342;">سرویس های ابری</span></strong><span style="color: #58595b;"><br>
+                    <span style="color: #aaaaaa; font-size: 14px;"><strong>CLOUD SERVICES</strong></span></span></p>
+            <p style="text-align: center;"><span style="color: #aaaaaa;">طیف وسیعی از نرم افزارها و سرویس های کارآمد<br>
+                    بر بستر ابری</span></p>
+
+        </div>
+    </div>
+    
+    <!-- image-2 -->
+    <div class="div-img-2">
+
+        <img src="images/moshvereh.jpg" style=" width:100%; height:100%;">
+
+    </div>
+    <!-- banner-->
+    <div class="banner-front">
+        <div class="row-banner">
+            <div class="column-banner">
+                <div class="" data-swiper-slide-index="0" style="width: 164px; margin-left: 12px;" role="group" aria-label="1 / 21">
+                    <div class="sp-lc-logo"><img src="https://greenweb.ir/wp-content/uploads/2021/03/chapiroos-Logo.png" data-src="" alt="چاپیروس" class="lazyDone"></div>
+                </div>
+            </div>
+            <div class="column-banner">
+                <div class="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="1" style="width: 164px; margin-left: 12px;" role="group" aria-label="2 / 21">
+                    <div class="sp-lc-logo"><img src="https://greenweb.ir/wp-content/uploads/2021/03/mana-logo.png" data-src="" alt="مانا اندیشه" class="lazyDone"></div>
+                </div>
+            </div>
+            <div class="column-banner">
+                <div class="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="2" style="width: 164px; margin-left: 12px;" role="group" aria-label="3 / 21">
+                    <div class="sp-lc-logo"><img src="https://greenweb.ir/wp-content/uploads/2021/03/iranserver-Logo.png" data-src="" alt="ایران سرور" class="lazyDone"></div>
+                </div>
+            </div>
+            <div class="column-banner">
+                <div class="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="3" style="width: 164px; margin-left: 12px;" role="group" aria-label="4 / 21">
+                    <div class="sp-lc-logo"><img src="https://greenweb.ir/wp-content/uploads/2021/03/Grrenweb-Cloud-Logo.png" data-src="" alt="گرین وب کلود" class="lazyDone"></div>
+                </div>
+            </div>
+            <div class="column-banner">
+                <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-prev" data-swiper-slide-index="4" style="width: 164px; margin-left: 12px;" role="group" aria-label="5 / 21">
+                    <div class="sp-lc-logo"><img src="https://greenweb.ir/wp-content/uploads/2021/03/greenTech-Logo.png" data-src="" alt="گرین تک" class="lazyDone"></div>
+                </div>
+            </div>
+            <div class="column-banner">
+                <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active" data-swiper-slide-index="5" style="width: 164px; margin-left: 12px;" role="group" aria-label="6 / 21">
+                    <div class="sp-lc-logo"><img src="https://greenweb.ir/wp-content/uploads/2021/03/Poshtiban-Logo.png" data-src="" alt="پشتیبان" class="lazyDone"></div>
+                </div>
+            </div>
+            <div class="column-banner">
+            <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-next" data-swiper-slide-index="6" style="width: 164px; margin-left: 12px;" role="group" aria-label="7 / 21">
+                                <div class="sp-lc-logo"><img src="https://greenweb.ir/wp-content/uploads/2021/03/faracloud-Logo.png" data-src="" alt="فراکلود" class="lazyDone"></div>
+                            </div>
+            </div>
+        </div>
+    </div>
+    <!-- banner -->
+
+    <div class="row">
+        <div class="column-2">
+
+            <img class="col-2-right" src="https://www.greenweb.ir/inc/uploads/2021/03/mehdimohammadi.png" data-src="" alt="مهدی محمدی، مدیرعامل هلدینگ دانش بنیان گرین وب" width="209" height="134">
+        </div>
+
+        <div class="column-2">
+            <div class="box">
+                <h2><span style="color:#ffffff;">ما به جایی وصل نیستیم؛ ما بـهـم وصـلـیـم</span></h2>
+            </div>
+        </div>
+    </div>
+    <!-- footer -->
+    <footer>
+        <div class="footer-1">
+            <div class="codevz-widget-about-content mb30"><strong>دفتر تهران:
+                </strong> میدان ولیعصر، خیابان شقایق، مرکز نوآوری و توسعه کسب و کارهای داده محور، طبقه چهارم <br>تلفن: 88853913 021<br>
+                <strong>دفتر مشهد:
+                </strong> بلوار خیام، بین چهارراه خیام و ارشاد، پارک فاوا، مرکز توسعه واحدهای فناور، طبقه دهم
+                <br>تلفن: 37638100 051
+                <div class="cz_elm social_header_2_right_2 inner_social_header_2_right_1" style="margin-top:10px;margin-bottom:10px;">
+                    <div class="cz_social cz_social_colored_hover cz_social_fx_10 cz_tooltip cz_tooltip_up"><a class="cz-facebook" href="https://www.facebook.com/greenwebco/" data-title="Facebook" target="_blank"><i class="fa fa-facebook"></i><span>Facebook</span></a><a class="cz-linkedin" href="https://www.linkedin.com/company/greenweb" data-title="Linkedin" target="_blank"><i class="fa fa-linkedin"></i><span>Linkedin</span></a><a class="cz-instagram" href="https://www.instagram.com/greenwebco/" data-title="Instagram" target="_blank"><i class="fa fa-instagram"></i><span>Instagram</span></a><a class="cz-twitter" href="https://twitter.com/greenwebir" data-title="Twitter" target="_blank"><i class="fa fa-twitter"></i><span>Twitter</span></a></div>
+                </div>
+
+                <a referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=275372&amp;Code=o5HwXiyr75aJk1jOeB8h"><img referrerpolicy="origin" src="https://Trustseal.eNamad.ir/logo.aspx?id=275372&amp;Code=o5HwXiyr75aJk1jOeB8h" data-src="" alt="" style="cursor:pointer" id="o5HwXiyr75aJk1jOeB8h" class="lazyDone" align="left"></a>
+
+                <img referrerpolicy="origin" id="jxlzwlaorgvjwlaorgvjrgvj" style="cursor:pointer" onclick="window.open(&quot;https://logo.samandehi.ir/Verify.aspx?id=143433&amp;p=rfthaodsxlaoaodsxlaoxlao&quot;, &quot;Popup&quot;,&quot;toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30&quot;)" alt="logo-samandehi" src="https://logo.samandehi.ir/logo.aspx?id=143433&amp;p=nbpdshwlqftishwlqftiqfti">
+            </div>
+        </div>
+        <div class="footer_2">
+            <div class="footer-2-col">
+                <a class="footer-2-tag ">تمامی حقوق مادی و معنوی برای هلدینگ دانش‌بنیان گرین‌وب محفوظ است. </a>
+            </div>
+            <div class="footer-2-col">
+                <a href="https://job.greenweb.ir/"><span class="footer-2-tag">فرصت‌های شغلی</span></a>
+            </div>
+
+        </div>
+
+    </footer>
+
+</body>
