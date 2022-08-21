25218e62828dcc0e134b012602ef461067192c9f
shadi <shd.yahoo.com>
2022-08-21 15:03:21 +0430

    google

---
+++ google/g2.php
@@ -0,0 +1,24 @@
+<div>
+    <div class="gb_7d gb_f gb_lg gb_cg" data-ogbl="">
+        <div class="gb_e gb_f"><a class="gb_d" data-pid="23" href="https://mail.google.com/mail/?authuser=0&amp;ogbl" target="_top">Gmail</a></div>
+        <div class="gb_e gb_f"><a class="gb_d" data-pid="2" href="https://www.google.com/imghp?hl=en&amp;authuser=0&amp;ogbl" target="_top">Images</a></div>
+    </div>
+</div>
+<div class="gb_Re">
+    <div class="gb_Sc">
+        <div class="gb_z gb_gd gb_f gb_zf" data-ogsr-fb="true" data-ogsr-alt="" id="gbwa">
+            <div class="gb_yf"><a class="gb_A" aria-label="Google apps" href="https://www.google.com/intl/en/about/products" aria-expanded="false" role="button" tabindex="0"><svg class="gb_Ue" focusable="false" viewBox="0 0 24 24">
+                        <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+                    </svg></a></div>
+        </div>
+    </div>
+    <div class="gb_Ma gb_gd gb_lg gb_f gb_zf">
+        <div class="gb_yf gb_Wa gb_lg gb_f"><a class="gb_A gb_La gb_f" aria-label="Google Account: Salamat pharmed  
+(salamatpharmed@gmail.com)" href="https://accounts.google.com/SignOutOptions?hl=en&amp;continue=https://www.google.com/" role="button" tabindex="0" aria-expanded="false"><img class="gb_Ba gbii" src="https://lh3.googleusercontent.com/ogw/AOh-ky3FoySzwoeJUjTb8I8A4ksCkyac8yelaISRbqTP=s32-c-mo" srcset="https://lh3.googleusercontent.com/ogw/AOh-ky3FoySzwoeJUjTb8I8A4ksCkyac8yelaISRbqTP=s32-c-mo 1x, https://lh3.googleusercontent.com/ogw/AOh-ky3FoySzwoeJUjTb8I8A4ksCkyac8yelaISRbqTP=s64-c-mo 2x " alt="" aria-hidden="true" data-noaft="" data-iml="1660121259035" data-atf="1" data-frt="0"></a>
+            <div class="gb_4a"></div>
+            <div class="gb_3a"></div>
+        </div>
+    </div>
+</div>
+<div style="overflow: hidden; position: absolute; top: 0px; visibility: hidden; width: 328px; z-index: 991; height: 0px; margin-top: 57px; transition: height 0.3s ease-in-out 0s; right: 0px; margin-right: 4px;"><iframe role="presentation" style="height: 100%; width: 100%; visibility: hidden;" scrolling="no" name="app" src="https://ogs.google.com/u/0/widget/app?origin=https%3A%2F%2Fwww.google.com&amp;cn=app&amp;pid=1&amp;spid=538&amp;hl=en" frameborder="0"></iframe></div>
+<div style="overflow: hidden; position: absolute; top: 0px; visibility: hidden; width: 372px; z-index: 991; height: 0px; margin-top: 57px; right: 0px; margin-right: 4px;"><iframe role="presentation" style="height: 100%; width: 100%; visibility: hidden;" scrolling="no" name="account" src="https://ogs.google.com/u/0/widget/account?origin=https%3A%2F%2Fwww.google.com&amp;cn=account&amp;pid=1&amp;spid=538&amp;hl=en" frameborder="0"></iframe></div>
---
+++ google/google.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GooGle</title>
+    <link rel="stylesheet" href="css\style.css">
+</head>
+
+<body>
+    <div style="overflow:auto">
+        <div>
+            <a class="h1" href="https://accounts.google.com/SignOutOptions?hl=en&amp;continue=https://www.google.com/">
+                <div>
+                    <img src="https://lh3.googleusercontent.com/ogw/AOh-ky3FoySzwoeJUjTb8I8A4ksCkyac8yelaISRbqTP=s32-c-mo" style=" border-radius: 15px">
+                </div>
+            </a>
+        </div>
+        <div>
+            <a class="h1" aria-label="Google apps" href="https://www.google.com/intl/en/about/products" aria-expanded="false" role="button" tabindex="0">
+                <svg class="" focusable="false" viewBox="0 0 24 24" style="height:20px;line-height:20px; width:20px; display: flex;">
+                    <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+                </svg>
+            </a>
+        </div>
+        <div>
+            <div class=""><a class="h1" href="https://www.google.com/imghp?hl=en&amp;authuser=0&amp;ogbl" target="_top">Images</a></div>
+        </div>
+        <div>
+            <div class=""><a class="h1" href="https://mail.google.com/mail/?authuser=0&amp;ogbl" target="_top">Gmail</a></div>
+        </div>
+
+    </div>
+
+    <div class="img">
+        <img src="images\google.png" alt="google">
+    </div>
+
+    <div>
+        <div class="divinput">
+
+            <!-- <div>
+                <span><svg class="svg" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="white-space: nowrap">
+                        <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                    </svg></span>
+            </div>  -->
+
+            <input type="text" id="myinput" name="q" class="myinput">
+        </div>
+        <div>
+            <form method="GET" action="http://www.google.com" class="form">
+                <div class="mydiv">
+                    <input type="submit" id="search" name="search" value="Google Search" class="mybtn"></input>
+                    <input type="submit" id="search2" name="search2" value="I'm Feeling Lucky" class="mybtn"></button>
+                </div> 
+
+            </form>
+        </div>
+            </div>
+            <div class=footer>
+                <div>Germany</div>
+            </div>
+</body>
+
+</html>
